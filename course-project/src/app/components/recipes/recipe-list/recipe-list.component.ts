c931e8d6770370da6e761630cdd0508e5f360b8d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-08 13:10:32 +0200

    recipe modal

--- course-project/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [];
 }
