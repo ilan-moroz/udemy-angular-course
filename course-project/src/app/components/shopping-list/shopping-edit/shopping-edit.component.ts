f377735a9af65655e36025ec65b5df5d6ad2e510
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-09 16:14:13 +0200

    add ingredient

--- course-project/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../../shared/ingredient.modal';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
 
+  @Output() addIngredients = new EventEmitter<Ingredient>();
+
+  addIngredient() {
+    this.addIngredients.emit({
+      name: this.nameInput.nativeElement.value,
+      amount: this.amountInput.nativeElement.value,
+    });
+  }
 }
