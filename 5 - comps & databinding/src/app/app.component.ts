c9db58e15e4e967b7c64d04b59b7c6300365d775
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-09 11:56:02 +0200

    EventEmitter

--- 5 - comps & databinding/src/app/app.component.ts
+++ 5 - comps & databinding/src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'test server', content: 'just a test' },
   ];
+
+  onServerAdded(data: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: data.serverName,
+      content: data.serverContent,
+    });
+  }
+
+  onBlueprintAdded(data: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: data.serverName,
+      content: data.serverContent,
+    });
+  }
 }
