519bc0a5308b9af8d7af337657b947c586e02ddb
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-09 19:17:00 +0200

    c

--- 7 - directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ 7 - directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -5,12 +5,15 @@ import {
   ElementRef,
   HostListener,
   HostBinding,
+  Input,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
@@ -21,6 +24,7 @@ export class BetterHighlightDirective implements OnInit {
     //     'backgroundColor',
     //     'blue'
     //   );
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
@@ -29,7 +33,7 @@ export class BetterHighlightDirective implements OnInit {
     //   'backgroundColor',
     //   'blue'
     // );
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
@@ -38,6 +42,6 @@ export class BetterHighlightDirective implements OnInit {
     //   'backgroundColor',
     //   'transparent'
     // );
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
