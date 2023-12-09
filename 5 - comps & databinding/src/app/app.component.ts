a8b005f0cbfe3e7c6d334098f1440d8a0b12fe60
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-09 13:08:33 +0200

    OnInit, OnChanges

--- 5 - comps & databinding/src/app/app.component.ts
+++ 5 - comps & databinding/src/app/app.component.ts
@@ -28,4 +28,8 @@ export class AppComponent {
       content: data.serverContent,
     });
   }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
 }
--- 5 - comps & databinding/src/app/server-element/server-element.component.ts
+++ 5 - comps & databinding/src/app/server-element/server-element.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  ViewEncapsulation,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,8 +14,26 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   // ensures only this component gets the css styles can be overwritten with none
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges {
   // Input property to receive data from a parent component
   // 'srvElement' is an alias for the property when used in binding
   @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log('ngOnInit called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 }
