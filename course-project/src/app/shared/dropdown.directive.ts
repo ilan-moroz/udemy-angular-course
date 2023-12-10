b867cff6a7668f37226bdf37ffa3a5766ee3053d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-10 11:08:33 +0200

    comments

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -1,15 +1,18 @@
 import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]', // Defines the directive, allowing it to be used as [appDropdown] in HTML elements
+  // Defines the directive, allowing it to be used as [appDropdown] in HTML elements
+  selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false; // Binds the 'open' CSS class to the element based on the value of isOpen
+  // Binds the 'open' CSS class to the element based on the value of isOpen
+  @HostBinding('class.open') isOpen = false;
 
   constructor() {}
 
+  // Event listener for the click event on the element
   @HostListener('click') onClick() {
-    // Event listener for the click event on the element
-    this.isOpen = !this.isOpen; // Toggles the isOpen state, which adds/removes the 'open' class
+    // Toggles the isOpen state, which adds/removes the 'open' class
+    this.isOpen = !this.isOpen;
   }
 }
