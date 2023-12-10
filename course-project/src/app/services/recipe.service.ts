76575b43b4573bcd9e3c8cdc92f276dace05636a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-10 17:59:50 +0200

    new Ingredient

--- course-project/src/app/services/recipe.service.ts
+++ course-project/src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../components/recipes/recipe.modal';
+import { Ingredient } from '../shared/ingredient.modal';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +10,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'Test',
-      'test recipe',
-      'https://marketplace.canva.com/EAFEGwki5iw/2/0/1067w/canva-white-yellow-clean-modern-brulle-cheese-cake-recipe-card-ZLMW2pkhjjg.jpg'
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
