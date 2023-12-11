c76c8561f8de56da144c60b11a0b0cdd02a51658
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-11 18:38:53 +0200

    2

--- 11 - routing/src/app/servers/edit-server/edit-server.component.ts
+++ 11 - routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -23,10 +24,9 @@ export class EditServerComponent implements OnInit {
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
 
-    console.log(this.route.snapshot.queryParams);
-    console.log(this.route.snapshot.fragment);
-
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe((qParams: Params) => {
+      this.allowEdit = qParams['allowEdit'] === '1' ? true : false;
+    });
     this.route.fragment.subscribe();
   }
 
