f2827d6c622f4d7d3f2a85b833816d60bad46db2
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-12 18:14:54 +0200

    edit recipe

--- course-project/src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,26 @@
+import { Recipe } from './../recipe.modal';
 import { Component } from '@angular/core';
+import { RecipeService } from '../../../services/recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrl: './recipe-edit.component.css'
+  styleUrl: './recipe-edit.component.css',
 })
 export class RecipeEditComponent {
+  id: number;
+  recipe: Recipe;
+  editMode = false;
 
+  constructor(private rService: RecipeService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      // this.recipe = this.rService.getRecipe(this.id);
+      console.log(this.editMode);
+    });
+  }
 }
