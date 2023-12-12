bb0f79244e5a8901f61e65280a4aa2c4583defb5
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-12 18:44:51 +0200

    unsubscribe

--- 13 - observables/src/app/home/home.component.ts
+++ 13 - observables/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private obsSub: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    this.obsSub = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.obsSub.unsubscribe();
+  }
 }
