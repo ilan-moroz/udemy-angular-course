b900df90800f63089d0d5b53855ba12d82472f09
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-12 19:58:55 +0200

    Subject

--- course-project/src/app/services/shopping-list.service.ts
+++ course-project/src/app/services/shopping-list.service.ts
@@ -1,11 +1,12 @@
+import { Subject } from 'rxjs';
 import { Ingredient } from './../shared/ingredient.modal';
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('apple', 5),
@@ -18,11 +19,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredient: Ingredient[]) {
     this.ingredients.push(...ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
