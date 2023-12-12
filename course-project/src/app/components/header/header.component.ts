5cb819737317e196c6f1f08d442fac0295a11ac9
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-12 13:17:16 +0200

    delete unused

--- course-project/src/app/components/header/header.component.ts
+++ course-project/src/app/components/header/header.component.ts
@@ -1,27 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-  @Output() navChange = new EventEmitter<{
-    showRecipe: boolean;
-    showList: boolean;
-  }>();
-
-  onRecipesClick() {
-    this.navChange.emit({
-      showRecipe: true,
-      showList: false,
-    });
-  }
-
-  onListClick() {
-    this.navChange.emit({
-      showRecipe: false,
-      showList: true,
-    });
-  }
-}
+export class HeaderComponent {}
