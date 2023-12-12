710cde8448fd6eecb9c89bb6645ee5e2d2b8998d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-12 19:34:54 +0200

    unsubscribe

--- 13 - observables/src/app/app.component.ts
+++ 13 - observables/src/app/app.component.ts
@@ -1,18 +1,25 @@
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activeSub: Subscription;
+
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe((active) => {
+    this.activeSub = this.userService.activatedEmitter.subscribe((active) => {
       this.userActivated = active;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.activeSub.unsubscribe();
   }
 }
