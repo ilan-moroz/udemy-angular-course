abdd819d1cee2a5d834775efb983ad40cc849a4d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-13 15:52:52 +0200

    comments

--- 15.1 - forms - reactive/src/app/app.component.ts
+++ 15.1 - forms - reactive/src/app/app.component.ts
@@ -9,15 +9,22 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
 
+  // Declaration of the FormGroup for the signup form
   signupForm: FormGroup;
 
   ngOnInit(): void {
+    // Initializing the FormGroup on component initialization
     this.signupForm = new FormGroup({
+      // Nested FormGroup for user data
       userData: new FormGroup({
+        // FormControl for username with required validator
         username: new FormControl(null, Validators.required),
+        // FormControl for email with required and email validators
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
+      // FormControl for gender, defaulting to 'male'
       gender: new FormControl('male'),
+      // FormArray for dynamic form controls
       hobbies: new FormArray([]),
     });
   }
@@ -26,10 +33,12 @@ export class AppComponent implements OnInit {
     console.log(this.signupForm);
   }
 
+  // Helper method to retrieve the FormControls from the hobbies FormArray
   getControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 
+  // Method to add a new FormControl to the hobbies FormArray
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
