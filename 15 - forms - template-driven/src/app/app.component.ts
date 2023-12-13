c31b1c0964e7825dbcc532e7f0cb3f3af073a16c
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-13 13:53:45 +0200

    default value

--- 15 - forms - template-driven/src/app/app.component.ts
+++ 15 - forms - template-driven/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('form') signForm: NgForm;
+  defaultQ = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
