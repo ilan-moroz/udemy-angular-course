3d6a11469452cb6049ffeb2d3bc151abb8f04100
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 15:10:34 +0200

    structure

--- 17 - pipes/src/app/sort.pipe.ts
+++ 17 - pipes/src/app/sort.pipe.ts
@@ -6,12 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortPipe implements PipeTransform {
   transform(value: any[], propName: string): any[] {
     return value.sort((a, b) => {
-      if (a[propName] < b[propName]) {
-        return -1;
-      }
-      if (a[propName] > b[propName]) {
-        return 1;
-      }
+      if (a[propName] < b[propName]) return -1;
+      if (a[propName] > b[propName]) return 1;
       return 0;
     });
   }
