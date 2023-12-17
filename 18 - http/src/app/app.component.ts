503f0d828d64e32bd9ac13f234f62c4e789bd6d9
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 17:24:13 +0200

    create service for http req

--- 18 - http/src/app/app.component.ts
+++ 18 - http/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { PostsService } from './posts.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
@@ -12,59 +13,25 @@ import { Post } from './post.model';
 export class AppComponent implements OnInit {
   @ViewChild('postForm') postForm: NgForm;
   loadedPosts: Post[] = [];
-  baseUrl =
-    'https://anuglar-http-91731-default-rtdb.europe-west1.firebasedatabase.app';
+
   isLoading = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private pService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.pService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
-    this.http
-      .post<{ name: string }>(this.baseUrl + '/posts.json', postData)
-      .subscribe((data) => {
-        console.log(data);
-      });
+    this.pService.createAndStorePost(postData);
     this.postForm.reset();
   }
 
   onFetchPosts() {
-    this.fetchPosts();
+    this.pService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isLoading = true;
-    // Making an HTTP GET request to fetch data
-    this.http
-      .get<{ [key: string]: Post }>(this.baseUrl + '/posts.json')
-      // Using 'pipe' to chain RxJS operators
-      .pipe(
-        // 'map' is used to transform the incoming data
-        map((responseData) => {
-          const postsArray: Post[] = [];
-          // Iterating over each key in the response object
-          for (const key in responseData) {
-            // Ensuring the key is a direct property of the response object
-            if (responseData.hasOwnProperty(key)) {
-              // Pushing a new Post object to the array
-              postsArray.push({ ...responseData[key], id: key });
-              // Each Post object includes properties from the response and the key as 'id'
-            }
-          }
-          return postsArray; // Returning the array of Post objects
-        })
-      )
-      // Subscribing to the Observable to receive the processed data
-      .subscribe((posts) => {
-        this.loadedPosts = posts;
-        this.isLoading = false;
-      });
   }
 }
---
+++ 18 - http/src/app/posts.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  baseUrl =
+    'https://anuglar-http-91731-default-rtdb.europe-west1.firebasedatabase.app';
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(postData: Post) {
+    this.http
+      .post<{ name: string }>(this.baseUrl + '/posts.json', postData)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  fetchPosts() {
+    // Making an HTTP GET request to fetch data
+    this.http
+      .get<{ [key: string]: Post }>(this.baseUrl + '/posts.json')
+      // Using 'pipe' to chain RxJS operators
+      .pipe(
+        // 'map' is used to transform the incoming data
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          // Iterating over each key in the response object
+          for (const key in responseData) {
+            // Ensuring the key is a direct property of the response object
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray; // Returning the array of Post objects
+        })
+      )
+      // Subscribing to the Observable to receive the processed data
+      .subscribe((posts) => {});
+  }
+}
