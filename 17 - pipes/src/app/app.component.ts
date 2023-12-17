b1df424788d11ed2c282da6ff8ab903c7aae4740
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 14:41:32 +0200

    filter pipe

--- 17 - pipes/src/app/app.component.ts
+++ 17 - pipes/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  filterStatus: string;
+
   servers = [
     {
       instanceType: 'medium',
