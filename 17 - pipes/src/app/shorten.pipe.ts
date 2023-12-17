1c574720a18a9c57db6d20b129d984128ccd9381
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 14:27:31 +0200

    custom pipe

---
+++ 17 - pipes/src/app/shorten.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorten',
+})
+export class ShortenPipe implements PipeTransform {
+  transform(value: any): string {
+    if (value.length > 0) return value.substr(0, 10) + '...';
+  }
+}
