6c6dcd6b23146165c97a0ad9b3c191ae5c5d15f4
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 12:12:59 +0200

    onDelete

--- course-project/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -52,4 +52,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.slForm.reset();
     this.editMode = false;
   }
+
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editIndex);
+    this.slForm.reset();
+    this.editMode = false;
+  }
 }
