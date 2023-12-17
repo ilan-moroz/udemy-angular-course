de8be08624f8749b7e819904b894a0b06e73169d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 11:37:35 +0200

    onEditItem

--- course-project/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ShoppingListService } from '../../../services/shopping-list.service';
 import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../../shared/ingredient.modal';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css',
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  sub: Subscription;
+  editMode = false;
+  editIndex: number;
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient);
+  }
+
+  ngOnInit(): void {
+    this.sub = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editIndex = index;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
