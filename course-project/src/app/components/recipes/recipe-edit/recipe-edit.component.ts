e1afab4a451147c1b96d121536fabf1f29e89681
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-17 12:34:18 +0200

    initForm

--- course-project/src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,7 @@ import { Recipe } from './../recipe.modal';
 import { Component } from '@angular/core';
 import { RecipeService } from '../../../services/recipe.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -12,6 +13,7 @@ export class RecipeEditComponent {
   id: number;
   recipe: Recipe;
   editMode = false;
+  recipeForm: FormGroup;
 
   constructor(private rService: RecipeService, private route: ActivatedRoute) {}
 
@@ -19,8 +21,26 @@ export class RecipeEditComponent {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-      // this.recipe = this.rService.getRecipe(this.id);
-      console.log(this.editMode);
+      this.initForm();
+    });
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImg = '';
+    let recipeDesc = '';
+
+    if (this.editMode) {
+      const recipe = this.rService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImg = recipe.imagePath;
+      recipeDesc = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImg),
+      description: new FormControl(recipeDesc),
     });
   }
 }
