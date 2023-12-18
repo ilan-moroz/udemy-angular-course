4e896d363e4df162a28074bf00d6c3329b9a2b60
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-18 15:19:07 +0200

    isAuth

--- course-project/src/app/components/header/header.component.ts
+++ course-project/src/app/components/header/header.component.ts
@@ -1,13 +1,27 @@
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 import { DataStorageService } from './../../services/data-storage.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  private userSub: Subscription;
+  isAuth = false;
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuth = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.saveRecipes();
@@ -15,5 +29,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
