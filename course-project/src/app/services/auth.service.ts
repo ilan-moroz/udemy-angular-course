33a2f342f58795f7438de91d24ec1c2217897c55
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-18 18:27:46 +0200

    ngOnInit

--- course-project/src/app/services/auth.service.ts
+++ course-project/src/app/services/auth.service.ts
@@ -71,6 +71,18 @@ export class AuthService {
     this.router.navigate(['/auth']);
   }
 
+  autoLogin() {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) return;
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+    if (loadedUser.token) this.user.next(loadedUser);
+  }
+
   private handAuth(
     email: string,
     token: string,
@@ -80,6 +92,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, id, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(err: HttpErrorResponse) {
