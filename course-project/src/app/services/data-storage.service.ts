6338e5f22112384a4e36bc780752c2e3a9b9b22d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-18 15:39:11 +0200

    comment

--- course-project/src/app/services/data-storage.service.ts
+++ course-project/src/app/services/data-storage.service.ts
@@ -27,20 +27,28 @@ export class DataStorageService {
 
   fetchRecipes() {
     return this.authService.user.pipe(
+      // Take the latest user value from authService's user Subject and complete
       take(1),
+      // Use exhaustMap to wait for the user Observable to complete
+      // Then, make an HTTP GET request with the user's token as a parameter
       exhaustMap((user) => {
         return this.http.get<Recipe[]>(this.baseUrl + '/recipes.json', {
           params: new HttpParams().set('auth', user.token),
         });
       }),
+      // Transform the incoming recipes using 'map'
       map((recipes) => {
+        // Iterate over each recipe and return an updated object
         return recipes.map((recipe) => {
+          // Use spread syntax to copy each recipe object
+          // Add an empty ingredients array if the property doesn't exist
           return {
             ...recipe,
             ingredients: recipe.ingredients ? recipe.ingredients : [],
           };
         });
       }),
+      // Perform a side effect with 'tap': update the recipeService
       tap((recipes) => {
         this.recipeService.setRecipes(recipes);
       })
