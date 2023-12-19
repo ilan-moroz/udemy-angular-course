6490700ae29110ec033fe54ef7996c0cf52c5a12
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-19 13:41:47 +0200

    RecipeRoutingModule

--- course-project/src/app/components/recipes/recipes.module.ts
+++ course-project/src/app/components/recipes/recipes.module.ts
@@ -6,6 +6,10 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { RecipeRoutingModule } from './recipes-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,11 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    RecipeRoutingModule,
   ],
 })
 export class RecipesModule {}
