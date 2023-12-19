18e0913ad6e64d532c207b9c7aaa04b301237269
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-19 11:22:52 +0200

    comments

--- course-project/src/app/components/auth/auth.guard.ts
+++ course-project/src/app/components/auth/auth.guard.ts
@@ -15,20 +15,25 @@ import { AuthService } from '../../services/auth.service';
 })
 export class AuthGuard {
   constructor(private authService: AuthService, private router: Router) {}
-
+  // 'canActivate' is a property of type CanActivateFn
   canActivate: CanActivateFn = (
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
+    route: ActivatedRouteSnapshot, // Information about the route being activated
+    state: RouterStateSnapshot // Router state snapshot
   ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
+    | Observable<boolean | UrlTree> // Returns an Observable of either boolean or UrlTree
+    | Promise<boolean | UrlTree> // Returns a Promise of either boolean or UrlTree
+    | boolean // Returns a boolean
+    // Returns a UrlTree
     | UrlTree => {
+    // Using RxJS 'pipe' to transform the user observable emitted by authService
     return this.authService.user.pipe(
+      // take just the last parameter and not subscribe to all changes
       take(1),
       map((user) => {
-        const isAuth = !!user;
-        if (isAuth) return true;
+        const isAuth = !!user; // Checking if the user object is truthy (authenticated)
+        if (isAuth) return true; // If authenticated, return true to allow navigation
+        // If not authenticated, redirect to the '/auth' route using createUrlTree
+        // createUrlTree generates a UrlTree based on the provided route path
         return this.router.createUrlTree(['/auth']);
       })
     );
