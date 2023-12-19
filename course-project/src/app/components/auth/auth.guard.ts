4e38ab5032dbf0794ca31677a8dfb02717234862
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-19 11:18:33 +0200

    AuthGuard

---
+++ course-project/src/app/components/auth/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateFn,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate: CanActivateFn = (
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree => {
+    return this.authService.user.pipe(
+      map((user) => {
+        const isAuth = !!user;
+        if (isAuth) return true;
+        return this.router.createUrlTree(['/auth']);
+      })
+    );
+  };
+}
