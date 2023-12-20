6124f1c4e62c26d5e33b93e25d8058b8aae487e2
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-20 12:37:44 +0200

    -

--- 24 - standalone comps/src/app/app.module.ts
+++ 24 - standalone comps/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { DetailsComponent } from './welcome/details/details.component';
 
@@ -10,6 +9,6 @@ import { DetailsComponent } from './welcome/details/details.component';
   declarations: [AppComponent, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, SharedModule, DetailsComponent],
+  imports: [BrowserModule, DetailsComponent],
 })
 export class AppModule {}
