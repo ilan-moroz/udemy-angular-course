88ce4d4ae9ecfc52b17265c2af6e0ea58e841349
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-20 12:34:20 +0200

    standalone: true,

--- 24 - standalone comps/src/app/app.module.ts
+++ 24 - standalone comps/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailsComponent } from './welcome/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
-  imports: [BrowserModule, SharedModule],
+  declarations: [AppComponent, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, DetailsComponent],
 })
 export class AppModule {}
