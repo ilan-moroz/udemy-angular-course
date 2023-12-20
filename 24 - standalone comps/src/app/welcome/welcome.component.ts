ab8188284f6ae6858017d58af9be74435b9deab4
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-20 13:03:37 +0200

    routing

--- 24 - standalone comps/src/app/welcome/welcome.component.ts
+++ 24 - standalone comps/src/app/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+
 import { DetailsComponent } from './details/details.component';
 
 @Component({
   standalone: true,
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
   imports: [DetailsComponent],
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {}
--- 24 - standalone comps/src/main.ts
+++ 24 - standalone comps/src/main.ts
@@ -1,11 +1,14 @@
-import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { AppRoutingModule } from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(AppRoutingModule)],
+});
