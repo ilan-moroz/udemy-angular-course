32374693d3b4b6c00308729da1296807bd8d9443
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-12-20 13:46:14 +0200

    signal update

--- 25 - angular signals/src/app/signals/signals.component.ts
+++ 25 - angular signals/src/app/signals/signals.component.ts
@@ -12,12 +12,12 @@ export class SignalsComponent {
   counter = signal(0);
 
   increment() {
-    this.counter++;
+    this.counter.update(oldCounter => oldCounter++);
     this.actions.push('INCREMENT');
   }
 
   decrement() {
-    this.counter--;
+    this.counter.update(oldCounter => oldCounter--);
     this.actions.push('DECREMENT');
   }
 }
